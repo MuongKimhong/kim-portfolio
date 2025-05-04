7258cb542bb7842b0856c372140916f736453c19
MuongKimhong <muongkimhong086@gmail.com>
2025-05-04 15:24:03 +1000

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: "/kim-portfolio/",
   transpileDependencies: true,
   lintOnSave: false,
   pluginOptions: {
